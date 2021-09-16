3b25748b0ac54abbfeea3677244536282d468491
Michael Ravedoni <michael@ravedoni.com>
2021-09-16 13:03:27 +0200

    up: disable verify

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -38,7 +38,9 @@ class ProductController extends Controller
      */
     public function getProducts()
     {
-        $req = Http::get(env('TCPOS_API_WOND_URL').'/getArticles');
+        $req = Http::withOptions([
+            'verify' => false,
+        ])->get(env('TCPOS_API_WOND_URL').'/getArticles');
         $response = $req->json();
         $data = data_get($response, 'getArticles.articleList');
         return $data;
