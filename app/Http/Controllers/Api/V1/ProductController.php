89bde840fee24bcdbde804ad6669bbc88dea0ac4
Michael Ravedoni <michael@ravedoni.com>
2021-09-17 13:59:24 +0200

    up: fix category null

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -240,6 +240,6 @@ class ProductController extends Controller
         if (in_array($notes2, ['Livres'])) {
             return "book";
         }
-        return $notes2;
+        return 'other';
     }
 }
