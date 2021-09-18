98b9f17cbdca91cc72c5e013119e39b207f52202
Michael Ravedoni <michael@ravedoni.com>
2021-09-18 14:09:03 +0200

    fix: update

--- app/Http/Controllers/Sync/ProductController.php
+++ app/Http/Controllers/Sync/ProductController.php
@@ -111,7 +111,7 @@ class ProductController extends Controller
                 // Update it
                 $data = $this->dataForWoo($tcposItem, $match);
                 //Product::update($match->_wooId, $data);
-                //SyncProductUpdate::dispatch($match->_wooId, $data);
+                SyncProductUpdate::dispatch($match->_wooId, $data);
                 $count_product_update += 1;
                 continue;
             } else {
