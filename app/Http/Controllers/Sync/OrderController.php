a6f656a32d731cf798575288277b5d206aed49d6
Michael Ravedoni <michael@ravedoni.com>
2021-09-20 17:02:15 +0200

    feat: order sync

--- app/Http/Controllers/Sync/OrderController.php
+++ app/Http/Controllers/Sync/OrderController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Codexshaper\WooCommerce\Facades\Customer;
 use Codexshaper\WooCommerce\Facades\Order;
-//use App\Jobs\SyncOrderCreate;
+use App\Jobs\SyncOrderUpdate;
 
 class OrderController extends Controller
 {
@@ -42,29 +42,23 @@ class OrderController extends Controller
             // - Has not been already synchronized (meta_tcposOrderId to null)
             // - Does not use voucher (bon cadeau) in the process (tcpos does not support voucher usage in order)
             if (empty($valueWooOrderIdMetaData) && $this->getVoucherCode($wooOrder) != 'chatelin') {
-
-                /*$metaDataArray = $wooOrder->meta_data;
-                metaDataArray[] = [config('cdv.wc_meta_tcpos_order_id') => null ];
-                $data = [
-                    'meta_data' => $metaDataArray,
-                ];
-                Order::update($wooOrder->id, $data);
-                */
+                // Create order in TCPOS
                 $this->createTcposOrder($wooOrder);
-
                 $count_order_create += 1;
             }     
             // - Order is in progress
             // - Has not been already synchronized (meta_tcposOrderId to null)
             // - Does use voucher (bon cadeau) in the process
             elseif (empty($valueWooOrderIdMetaData) && $this->getVoucherCode($wooOrder) == 'chatelin') {
-                //dd($wooOrder);
+                // Nothing
                 $count_order_manual += 1;
             } else {
                 // Nothing
                 $count_order_untouched += 1;
             }
         }
+
+        activity()->withProperties(['group' => 'sync', 'level' => 'start', 'resource' => 'orders'])->log('Orders sync started | '.$wooOrders->count().' processing orders found, '.$count_order_create.' to create, '.$count_order_manual.' to be processed manually and '.$count_order_untouched.' untouched.');
 
         return response()->json([
             'message' => 'Sync queued. See /jobs.',
@@ -91,14 +85,31 @@ class OrderController extends Controller
     public function createPaymentTcposData($wooOrder)
     {
         $paymentConfig = data_get(config('cdv.payment_methods'), data_get($wooOrder, 'payment_method'));
-        $paymentData = [
-            'payments' => [
-                'paymentType' => data_get($paymentConfig, 'type'),
-                'paymentNotes' => data_get($paymentConfig, 'note'),
-                'paymentAmount' => data_get($wooOrder, 'total'),
-            ]
-        ];
-        dd($paymentData);
+
+        if (isset($paymentConfig)) {
+            $paymentData = [
+                'payments' => [
+                    [
+                        'paymentType' => data_get($paymentConfig, 'type'),
+                        'paymentNotes' => data_get($paymentConfig, 'note'),
+                        'paymentAmount' => data_get($wooOrder, 'total'),
+                    ]
+                ]
+            ];
+        } else {
+            $paymentData = [
+                'payments' => [
+                    [
+                        'paymentType' => 'cash',
+                        'paymentNotes' => 'Non payé. Paiement créé artificiellement pour un test.',
+                        'paymentAmount' => data_get($wooOrder, 'total'),
+                    ]
+                ]
+            ];
+            //$paymentData = [];
+            activity()->withProperties(['group' => 'sync', 'level' => 'warning', 'resource' => 'orders'])->log('The order payment #'.$wooOrder->id.' has no payment. A fake one has benn created for testing purpose.');
+        }
+        return $paymentData;
     }
 
     /**
@@ -128,7 +139,6 @@ class OrderController extends Controller
                 'orderType' => config('cdv.default_order_type'),
                 'priceLevelId' => config('cdv.default_price_level_id'),
                 'total' => $wooOrder->total,
-                'transactionCausalId' => $wooOrder->id,
                 'transactionComment' => 'Commande Woocommerce #'.$wooOrder->id.' à livrer chez '.$stringShippingAddress,
                 'itemList' => $items,
             ]
@@ -140,13 +150,13 @@ class OrderController extends Controller
 
         // S'il y a a un token: créer la commande dans TCPOS
         if ($token) {
-            $requestOrder = Http::get(env('TCPOS_API_WOND_URL').'/calculateOrder?token='.urlencode($token).'&data='.json_encode($data));
+            $requestOrder = Http::get(env('TCPOS_API_WOND_URL').'/createOrder?token='.urlencode($token).'&data='.urlencode(json_encode($data)));
             $dataOrder = $requestOrder->json();
-            $dataOrderResponse = data_get($dataOrder, 'calculateOrder.result');
+            $dataOrderResponse = data_get($dataOrder, 'createOrder.result');
 
             // S'il y a une erreur dans la création de la commande
             if ($dataOrderResponse != 'OK') {
-                activity()->withProperties(['group' => 'sync', 'level' => 'error', 'resource' => 'orders'])->log('The order could not be transmitted correctly to TCPOS WOND');
+                activity()->withProperties(['group' => 'sync', 'level' => 'error', 'resource' => 'orders'])->log('The order could not be transmitted correctly to TCPOS WOND. Message: '.data_get($dataOrderConfirm, 'createOrder.message'));
                 return 'Error: The order could not be transmitted correctly to TCPOS WOND';
             }
 
@@ -154,19 +164,31 @@ class OrderController extends Controller
             $requestOrderConfirm = Http::get(env('TCPOS_API_WOND_URL').'/confirmOrder', [
                 'token' => $token,
                 'shopId' => config('cdv.default_shop_id'),
-                'guid' => null,
+                'guid' => data_get($dataOrder, 'createOrder.data.guid'), // Get guid from the previous request
                 'operation' => config('cdv.default_confirm_order_operation'),
-                'payments' => $this->createPaymentTcposData(),
+                'payments' => json_encode($this->createPaymentTcposData($wooOrder)),
             ]);
             $dataOrderConfirm = $requestOrderConfirm->json();
             $dataOrderConfirmResponse = data_get($dataOrderConfirm, 'confirmOrder.result');
 
-            // S'il y a une erreur dans la création de la commande
+            // S'il y a une erreur dans la confirmation de la commande
             if ($dataOrderConfirmResponse != 'OK') {
-                activity()->withProperties(['group' => 'sync', 'level' => 'error', 'resource' => 'orders'])->log('Error: The order could not be confirmed correctly to TCPOS WOND');
+                activity()->withProperties(['group' => 'sync', 'level' => 'error', 'resource' => 'orders'])->log('Error: The order could not be confirmed correctly to TCPOS WOND. Message: '.data_get($dataOrderConfirm, 'confirmOrder.message'));
                 return 'Error: The order could not be confirmed correctly to TCPOS WOND';
             }
-            dd($dataOrderConfirm);
+            activity()->withProperties(['group' => 'sync', 'level' => 'info', 'resource' => 'orders'])->log('Order created in TCPOS WOND');
+
+            // S'il y a une erreur dans la confirmation de la commande : mettons un id sur la commande de Woocommerce
+            $wooUpdateOrderData = [
+                'meta_data' => [
+                    ['key' => config('cdv.wc_meta_tcpos_order_id'), 'value' => data_get($dataOrder, 'createOrder.data.guid')]
+                ]
+            ];
+            // Order::update($this->id, $this->data);
+            SyncOrderUpdate::dispatch($wooOrder->id, $wooUpdateOrderData);
+            activity()->withProperties(['group' => 'sync', 'level' => 'info', 'resource' => 'orders'])->log('Order dispatched. See /jobs');
+        } else {
+            activity()->withProperties(['group' => 'sync', 'level' => 'error', 'resource' => 'orders'])->log('Token could not be created.');
         }
     }
 }
---
+++ app/Jobs/SyncOrderUpdate.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use romanzipp\QueueMonitor\Traits\IsMonitored;
+use Codexshaper\WooCommerce\Facades\Order;
+
+class SyncOrderUpdate implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use IsMonitored;
+
+    public $id;
+    public $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($id, $data)
+    {
+        $this->id = $id;
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {   
+        // https://codexshaper.github.io/docs/laravel-woocommerce/#update-order
+        Order::update($this->id, $this->data);
+
+        activity()->withProperties(['group' => 'sync', 'level' => 'info', 'resource' => 'orders'])->log('Order updated in Woocommerce : '.$this->id);
+    }
+}
