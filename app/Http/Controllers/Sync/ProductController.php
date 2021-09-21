3b8a9f9decf0da4c82c1a9d6ea0f5eab5cfe0e2e
Michael Ravedoni <michael@ravedoni.com>
2021-09-21 14:44:04 +0200

    test: image sync

--- app/Http/Controllers/Sync/ProductController.php
+++ app/Http/Controllers/Sync/ProductController.php
@@ -86,7 +86,7 @@ class ProductController extends Controller
         // The loop: from tcpos to Woo
         foreach ($tcposResources as $tcposItem) {
             // For testing only a product
-            //if ($tcposItem->_tcposCode != 6558) {continue;}
+            if ($tcposItem->_tcposCode != 6578) {continue;}
 
             $match = Woo::where('resource', 'product')->where('_tcposCode', $tcposItem->_tcposCode)->first();
 
@@ -215,6 +215,7 @@ class ProductController extends Controller
             ];
         }
         $dist_image_url = env('TCPOS_PRODUCTS_IMAGES_BASE_URL').$tcposProduct->_tcposId.'.jpg';
+        dd($dist_image_url);
         //instead of $tcposProduct->imageUrl()
         if (isset($wooProduct) && $tcposProduct->imageUrl() != null) {
             //There is a tcpos image and a wooProduct
@@ -223,6 +224,7 @@ class ProductController extends Controller
                 if (data_get($wooProduct->data, 'images.0.name') != $tcposProduct->imageHash) {
                     //There is an existing image but not match the tcpos hash one : upload
                     $images = [['name' => $tcposProduct->imageHash, 'src' => $dist_image_url]];
+                    activity()->withProperties(['group' => 'sync', 'level' => 'info', 'resource' => 'products'])->log('Product image URL : '. $dist_image_url);
                 } else {
                     //There is an existing image that match the tcpos hash one : keep it
                     $images = $wooProduct->data->images;
@@ -231,6 +233,7 @@ class ProductController extends Controller
             if (data_get($wooProduct->data, 'images.0.name') == null) {
                 //There is no existing image : upload
                 $images = [['name' => $tcposProduct->imageHash, 'src' => $dist_image_url]];
+                activity()->withProperties(['group' => 'sync', 'level' => 'info', 'resource' => 'products'])->log('Product image URL : '. $dist_image_url);
             }
         } else {
             //There is no tcpos image or no wooProduct
