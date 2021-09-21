76684569ac528283b0630b37cd09c746d66dc64e
Michael Ravedoni <michael@ravedoni.com>
2021-09-21 20:34:17 +0200

    up: sync cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('import:woo')->timezone('Europe/Zurich')->between('6:00', '19:00')->hourlyAt(1);
-        $schedule->command('import:tcpos')->timezone('Europe/Zurich')->between('6:00', '19:00')->hourlyAt(5);
+        $schedule->command('import:tcpos')->timezone('Europe/Zurich')->between('6:00', '19:00')->hourlyAt(2);
         $schedule->command('sync:tcpos_woo')->timezone('Europe/Zurich')->between('6:00', '19:00')->hourlyAt(25);
         $schedule->command('sync:tcpos_woo_order')->timezone('Europe/Zurich')->between('6:00', '19:00')->everyThirtyMinutes();
 
