906fe68cd58737ef7dfd7f38a39829e868a03507
Michael Ravedoni <30499597+michaelravedoni@users.noreply.github.com>
2025-11-27 10:15:44 +0100

    up: order check notification every 4 hours

--- app/Console/Commands/Check.php
+++ app/Console/Commands/Check.php
@@ -3,9 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Utilities\AppHelper;
+use Illuminate\Support\Carbon;
 use App\Mail\OrderProblemCheck;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
 use Codexshaper\WooCommerce\Facades\Order;
 
 class Check extends Command
@@ -42,14 +44,27 @@ class Check extends Command
         $this->line('Check if everthing is ok');
 
         // Detect if there is a problem with orders
+        $problematicOrders = [];
         $orders = Order::all(['per_page' => 5, 'page' => 1]);
         foreach ($orders as $order) {
             $tcposOrderId = AppHelper::getMetadataValueFromKey($order->meta_data, config('cdv.wc_meta_tcpos_order_id'));
-            if (empty($tcposOrderId) && in_array($order->status, ['processing', 'on-hold']) && \Carbon\Carbon::parse($order->date_created) < \Carbon\Carbon::now()->addMinutes(60)) {
-                // problème de synchronisation détecté
-                $this->line('Order sync problem detected.');
-                Mail::to('charpin@chateaudevilla.ch')->send(new OrderProblemCheck);
-                activity()->withProperties(['group' => 'email', 'level' => 'warning', 'resource' => 'orders'])->log('Problem detected for Order #'.$order->id.'. Email sent.');
+            if (empty($tcposOrderId) && in_array($order->status, ['processing', 'on-hold']) && Carbon::parse($order->date_created) < Carbon::now()->addMinutes(60)) {
+                $problematicOrders[] = $order->id;
+            }
+        }
+
+        if (! empty($problematicOrders)) {
+            $this->line('Order sync problem detected for orders: ' . implode(', ', $problematicOrders));
+
+            $lastSent = Cache::get('order_problem_check_sent_at');
+
+            if (! $lastSent || Carbon::parse($lastSent)->lt(now()->subHours(4))) {
+                Mail::to('charpin@chateaudevilla.ch')->send(new OrderProblemCheck());
+                Cache::put('order_problem_check_sent_at', now(), now()->addHours(4));
+                activity()->withProperties(['group' => 'email', 'level' => 'warning', 'resource' => 'orders', 'orders' => $problematicOrders])->log('Problem detected for Orders. Email sent.');
+                $this->line('Order problem email sent.');
+            } else {
+                $this->line('An email has been sent less than 4 hours ago. Waiting before sending a new one.');
             }
         }
 
